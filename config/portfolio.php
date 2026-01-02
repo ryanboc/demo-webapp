6fb2acf43fba05e2b03b5a6257ab86bc6be173eb
coderBockie <rboc12@gmail.com>
2026-01-02 10:26:41 +1000

    Add photo

--- config/portfolio.php
+++ config/portfolio.php
@@ -3,10 +3,11 @@
 return [
     'name' => env('PORTFOLIO_NAME', 'Ryan Boc'),
     'initials' => env('PORTFOLIO_INITIALS', 'YN'),
-    'email' => env('PORTFOLIO_EMAIL', 'you@email.com'),
+    'email' => env('PORTFOLIO_EMAIL', 'rboc12@gmail.com'),
     'location' => env('PORTFOLIO_LOCATION', 'Brisbane / Toowoomba / Remote'),
     'headline' => env('PORTFOLIO_HEADLINE', 'I build polished products that feel effortless.'),
     'subheadline' => env('PORTFOLIO_SUBHEADLINE', 'I build secure APIs, manage Linux servers, and optimize backend performance.'),
-    'github' => env('PORTFOLIO_GITHUB', 'https://github.com/'),
-    'linkedin' => env('PORTFOLIO_LINKEDIN', 'https://www.linkedin.com/'),
+    'github' => env('PORTFOLIO_GITHUB', 'https://github.com/ryanboc'),
+    'linkedin' => env('PORTFOLIO_LINKEDIN', 'https://www.linkedin.com/in/boc-the-coder/'),
+    'photo' => env('PORTFOLIO_PHOTO', '/img/8053598.jpeg'),
 ];
